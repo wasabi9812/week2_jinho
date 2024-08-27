3d1a27644f093369a3159552dbb6e5586edf4080
wasabi9812 <68814587+wasabi9812@users.noreply.github.com>
2024-08-27 21:19:36 +0900

    [Gold V] Title: 동전, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.py
+++ 백준/Gold/9084. 동전/동전.py
@@ -1,15 +1,15 @@
 import sys
 
-T = int(sys.stdin.readline())
-for i in range(T):
-    N = int(sys.stdin.readline())
+T = int(sys.stdin.readline().strip())
+for _ in range(T):
+    N = int(sys.stdin.readline().strip())
     coins = list(map(int,sys.stdin.readline().split()))
-    M = int(sys.stdin.readline())
-    # print(coins)
-    dp = [0]*(M+1)
-    dp[0] = 1
-
+    M = int(sys.stdin.readline().strip())
+    
+    dp = [0 for _ in range(M+1)]
+    dp[0]=1
     for coin in coins:
         for i in range(coin,M+1):
-            dp[i] +=dp[i-coin]
+            dp[i]+=dp[i-coin]
+            
     print(dp[M])
